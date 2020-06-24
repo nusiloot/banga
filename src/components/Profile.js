899bb2525c3090789d068cdd920522ff8a85a34f
jenaye <stahlilistenvoid@gmail.com>
2020-06-24 23:27:06 +0200

    fix

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -30,11 +30,11 @@ export const Profile = () => (
 
     I'm an IT engineer and I'm working as a pentester for Orange Cyberdefense.<br/>
 
-    I am a contributor to the <a href="http://rtfm.re/">RTFM</a> association, especially to the challenges creations part of the <a href="https://twitter.com/sigsegv_event">Sigvsegv event</a>.<br/>
+    I am a contributor to the <a target='_blank' rel='noopener noreferrer' href="http://rtfm.re/">RTFM</a> association, especially to the challenges creations part of the <a href="https://twitter.com/sigsegv_event">Sigvsegv event</a>.<br/>
 
-    I also contribute to several open source projects, such as this <a href="https://inventory.rawsec.ml/tools.html">inventory</a> of cybersecurity oriented tools.<br/>
+    I also contribute to several open source projects, such as this <a target='_blank' rel='noopener noreferrer'  href="https://inventory.rawsec.ml/tools.html">inventory</a> of cybersecurity oriented tools.<br/>
 
-    I am also a member of the CTF team <a href="https://inshallhack.org/">Inshallhack</a><br/>
+    I am also a member of the CTF team <a target='_blank' rel='noopener noreferrer'  href="https://inshallhack.org/">Inshallhack</a><br/>
 
     <br/>
     <Card className='col-12 col-sm-10 col-md-6 p-0'>
@@ -43,7 +43,7 @@ export const Profile = () => (
         {
           links.map((item, key) => (
             <ListGroup.Item key={key}>
-              <a href={ item.link } className={item.class || ''}>{ item.name }</a>
+              <a target='_blank' rel='noopener noreferrer' href={ item.link } className={item.class || ''}>{ item.name }</a>
             </ListGroup.Item>
           ))
         }
@@ -55,7 +55,7 @@ export const Profile = () => (
       Contact by email jenaye[@]protonmail.com
     </Alert>
     <Alert variant="dark">
-      You can push pull request if you find bugs :  <a href="https://github.com/jenaye/banga">here</a>
+      You can push pull request if you find bugs :  <a target='_blank' rel='noopener noreferrer' href="https://github.com/jenaye/banga">here</a>
     </Alert>
   </Jumbotron>
   </>
